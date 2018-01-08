5c8d4ebf3c9cb3a57c99e78dabd70dfbad3cf7d2
Alba Mendoza <albamariams97@gmail.com>
2018-01-08 20:32:48 +0000

    upload modificaciones p

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 //  alert('El árbol está listo');
 $( ".parrafoCuatroDivDos" ).css( "backgroundColor", "purple" );
+$(".parrafoDos").replaceWith("<p>Esto ha cambiado</p>");
 });
 
 var parrafoCuatro = document.getElementsByTagName('p')[3];
+var parrafoDos = document.getElementsByTagName('p')[1];
 
+parrafoDos.innerHTML = 'Esto ha cambiado';
 parrafoCuatro.style.backgroundColor = '#fb0808';
