de4b7d0da22633278c5abc374ce2ede778517c29
Alba Mendoza <albamariams97@gmail.com>
2018-01-08 20:47:28 +0000

    añado enlace parrafo 3 vanilla js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
 //  alert('El árbol está listo');
 $( ".parrafoCuatroDivDos" ).css( "backgroundColor", "purple" );
-$(".parrafoDos").replaceWith("<p>Esto ha cambiado</p>");
+$(".parrafoDosDivDos").replaceWith("<p>Esto ha cambiado</p>");
 });
 
 var parrafoCuatro = document.getElementsByTagName('p')[3];
 var parrafoDos = document.getElementsByTagName('p')[1];
+var parrafoTres = document.getElementsByTagName('p')[2];
+var parrafoTres = document.getElementsByTagName('p')[2];
+var enlace = document.createElement('a');
+var contenidoEnlace = document.createTextNode('Google');
 
+enlace.appendChild(contenidoEnlace);
+enlace.href = 'https://www.google.es';
+parrafoTres.appendChild(enlace);
+parrafoTres.href = 'https://www.google.com/';
 parrafoDos.innerHTML = 'Esto ha cambiado';
 parrafoCuatro.style.backgroundColor = '#fb0808';
